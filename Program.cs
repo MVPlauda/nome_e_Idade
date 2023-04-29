d17c5e1589c1545201816471dfa513a0d77f6ebb
MVPlauda <plaudadev@gmail.com>
2023-04-29 01:31:15 -0300

    nome_e_idade

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //WriteLine é na linha de baixo
+        //Write na mesma linha
+
+        Console.Write("Digite seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite sua idade: ");
+        string idade = Console.ReadLine();
+
+        Console.WriteLine($"Olá {nome}, você tem {idade} anos.");
+    }
+
+}
